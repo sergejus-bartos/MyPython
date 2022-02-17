de1b40420b86338047a12fe6de4a9db2f49491ab
sergejus-bartos <sergejus.bartos@inbox.lt>
2022-02-17 16:01:21 +0200

    some more

--- first.py
+++ first.py
@@ -1,1 +1,23 @@
-print("hi there")
+# print("hi there")
+# print(len('apple'))
+# fruit = 'apple'
+# print(fruit.lower())
+
+# input('press Enter')
+
+# The cost of one server per hour.
+# cost_per_hour = 0.51
+# Compute the costs for one server.
+# cost_per_day = 24 * cost_per_hour
+# cost_per_month = 30 * cost_per_day
+# Display the results.
+# print('Cost to operate one server per day is ${:.2f}.'.format(cost_per_day))
+# print('Cost to operate one server per month is ${:.2f}.'.format(cost_per_month))
+from pickle import TRUE
+
+
+a = 37
+b = 36
+c = TRUE
+d = c and (a>b)
+print (d)
