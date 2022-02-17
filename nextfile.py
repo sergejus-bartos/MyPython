1e5065c1cbc2eaa27785f2001f7d4438e32ddbf4
sergejus-bartos <sergejus.bartos@inbox.lt>
2022-02-17 18:37:03 +0200

    more learning

--- nextfile.py
+++ nextfile.py
@@ -1,1 +1,19 @@
 #next file
+
+
+
+# some_animals = animals[1:3]
+# print(format(some_animals))
+
+animals = ['me', 'you', 'him']
+animals.extend(['her', 'some'])
+print(animals[-1])
+try:
+    cat_index = animals.index('cat')
+except: 
+    cat_index = 'no cats'    
+print(cat_index)
+
+for animal in animals:
+    print(animal.upper())
+    
